6561e4829db1a2256b6078ec213b062eece28848
jowelahmedhim <jowelahmedhim@gmail.com>
2021-05-26 11:47:43 +0600

    VoiceRecorder function added in activity_add_note.xml

--- app/src/main/java/com/example/pencil/AddNoteActivity.java
+++ app/src/main/java/com/example/pencil/AddNoteActivity.java
@@ -16,41 +16,58 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddNoteActivity extends AppCompatActivity {
 
+
+    private RelativeLayout relativeLayout;
     private ImageView noteImageIv;
 
     private Toolbar toolbar;
     private BottomNavigationView navigationView;
+    private BottomSheetDialog bottomSheetDialog;
 
     private ProgressDialog progressDialog;
 
-
-
     private String[] cameraPermission;
     private String[] storagePermission;
+    private String[] recordingPermission;
 
     private static final int CAMERA_REQUEST_CODE = 200;
     private static final int STORAGE_REQUEST_CODE = 300;
     private static final int IMAGE_PICK_GALLERY_CODE = 400;
     private static final int IMAGE_PICK_CAMERA_CODE = 500;
+    private static final int RECORD_AUDIO_REQUEST_CODE =600;
 
     private Uri imageUri;
     private String selectedImagePath;
+
+    private MediaRecorder mediaRecorder;
+    private boolean isRecording = false;
+
 
 
     @Override
@@ -96,9 +113,12 @@ public class AddNoteActivity extends AppCompatActivity {
                         showImagePickerDialog();
                         return true;
                     case R.id.addPaint:
-
                         Intent intent = new Intent(AddNoteActivity.this,PaintActivity.class);
                         startActivity(intent);
+                        return true;
+
+                    case R.id.addVoice:
+                        showBottomSheetDialog();
                         return true;
                 }
                 return false;
@@ -106,30 +126,19 @@ public class AddNoteActivity extends AppCompatActivity {
         });
 
     }
-
     private void init() {
+        relativeLayout = findViewById(R.id.addNoteBackground);
         noteImageIv = findViewById(R.id.noteImage);
     }
 
-    private boolean checkCameraPermission(){
-        boolean result = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA)==(PackageManager.PERMISSION_GRANTED);
-        boolean result1 = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
-        return result && result1;
-    }
-    private void requestCameraPermission(){
-        ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_REQUEST_CODE);
-    }
-    private boolean checkStoragePermission(){
-        boolean result1 = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
-        return result1;
-    }
-    private void requestStoragePermission(){
-        ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_REQUEST_CODE);
+
+    private void initPermission() {
+        cameraPermission = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        recordingPermission = new String[]{Manifest.permission.RECORD_AUDIO};
     }
 
+    //function of image taking note
     private void showImagePickerDialog() {
         String[] option = {"Camera","Gallery"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -143,7 +152,7 @@ public class AddNoteActivity extends AppCompatActivity {
                             {
                                 //camera permission allowed
                                 pickFromCamera();
-                                
+
                             }else {
                                 //camera permission not allowed
                                 requestCameraPermission();
@@ -172,10 +181,124 @@ public class AddNoteActivity extends AppCompatActivity {
         startActivityForResult(intent,IMAGE_PICK_CAMERA_CODE);
     }
 
-    private void initPermission() {
-        cameraPermission = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
-        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    //function of voice recording note
+    private void showBottomSheetDialog() {
+
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.add_note_bottom_sheet_dialog);
+        bottomSheetDialog.setCancelable(false);
+
+        ImageButton playBtn,cancelBtn,okBtn,pauseBtn;
+        Chronometer timer;
+        timer = bottomSheetDialog.findViewById(R.id.chronometers);
+        playBtn=bottomSheetDialog.findViewById(R.id.play_btn);
+        pauseBtn = bottomSheetDialog.findViewById(R.id.pause_btn);
+        cancelBtn = bottomSheetDialog.findViewById(R.id.cancel_btn);
+        okBtn = bottomSheetDialog.findViewById(R.id.ok_btn);
+        bottomSheetDialog.show();
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    if (checkAudioRecordPermission())
+                    {
+                        startRecording();
+                        isRecording = true;
+                        timer.setBase(SystemClock.elapsedRealtime());
+                        timer.start();
+                    }else {
+                        requestAudioRecordPermission();
+                    }
+
+            }
+        });
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.stop();
+                isRecording = false;
+                stopRecording();
+            }
+        });
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isRecording){
+                    stopRecording();
+
+                }else {
+                    bottomSheetDialog.dismiss();
+                }
+            }
+        });
+
     }
+
+    private void startRecording() {
+
+        String getFilePath = this.getExternalFilesDir("/").getAbsolutePath();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy_MM_dd_hh_mm_ss", Locale.getDefault());
+        Date date = new Date();
+        String recordFile =  "Recording_"+simpleDateFormat.format(date)+".3gp";
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        mediaRecorder.setOutputFile(getFilePath+"/"+recordFile);
+
+        try {
+            mediaRecorder.prepare();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mediaRecorder.start();
+    }
+
+    private void stopRecording() {
+
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+        bottomSheetDialog.dismiss();
+    }
+
+
+    //checking all permission & request permission
+    private boolean checkCameraPermission(){
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)==(PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+    private void requestCameraPermission(){
+        ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_REQUEST_CODE);
+    }
+    private boolean checkStoragePermission(){
+        boolean result1 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+        return result1;
+    }
+    private void requestStoragePermission(){
+        ActivityCompat.requestPermissions(this,storagePermission,CAMERA_REQUEST_CODE);
+    }
+    private boolean checkAudioRecordPermission() {
+
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)==(PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+    private void requestAudioRecordPermission(){
+        ActivityCompat.requestPermissions(this,recordingPermission,RECORD_AUDIO_REQUEST_CODE);
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -217,6 +340,21 @@ public class AddNoteActivity extends AppCompatActivity {
                 }
             }
             break;
+            case RECORD_AUDIO_REQUEST_CODE:{
+                if (grantResults.length>0){
+                    boolean audioRecordAccepted= grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (audioRecordAccepted){
+                        //permission granted
+
+                    }else {
+                        //permission denied
+                        Toast.makeText(this,"Audio permission necessary..",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+
+
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
